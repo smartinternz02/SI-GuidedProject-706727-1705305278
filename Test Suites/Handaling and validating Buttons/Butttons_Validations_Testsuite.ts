af1310906340574a0ac2209b352e3b959652d892
NikitaDangwal <156332091+NikitaDangwal@users.noreply.github.com>
2024-02-05 20:19:47 +0530

    Handling and Validating Buttons

---
+++ Test Suites/Handaling and validating Buttons/Butttons_Validations_Testsuite.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Butttons_Validations_Testsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>28b9c194-786e-4915-b1c3-9336813798af</testSuiteGuid>
+   <testCaseLink>
+      <guid>c86bfcf2-0301-4a95-b14a-337aa55a1834</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Handaling and Validating Buttons/Handaling and Validating_CheckBoxes</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b01b60fe-dd69-47df-83b9-8cceda148c3d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Handaling and Validating Buttons/Handling and validating _Dropdown_Buttons</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>b45c4d89-5c83-4904-9722-e1629b2ab155</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f2f0d1de-71b5-44c7-848c-7dafde6086de</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Handaling and Validating Buttons/Handling and Validating_InputButtons</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>417555d1-3f4b-4cbd-acaf-214bab5891fd</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>377cacbd-7423-4c67-aec0-c73949511b20</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
