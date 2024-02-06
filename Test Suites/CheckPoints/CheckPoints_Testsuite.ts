2baae828439122809d222be45d973796348e09d2
NikitaDangwal <156332091+NikitaDangwal@users.noreply.github.com>
2024-02-06 09:15:49 +0530

    CheckPoints

---
+++ Test Suites/CheckPoints/CheckPoints_Testsuite.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>CheckPoints_Testsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>fb34cb99-2a3d-4ae0-8295-f0ea6c994471</testSuiteGuid>
+   <testCaseLink>
+      <guid>8e2881f3-c058-431f-a0a5-5c300558ced9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CheckPoints/AppointmentConfirmation_Testdata_CheckPoint</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e06e896a-bbb0-4138-a3a4-1fb41476bd01</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CheckPoints/CheckPoint_Excel_001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
